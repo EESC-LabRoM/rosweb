efef76c0ff377489a0c5b4b4422fb81636ed44d6
Marco Arruda <marco.nc.arruda@gmail.com>
2016-06-25 18:48:51 -0300

    browserify

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,32 @@
 var gulp = require("gulp");
 
 // TypeScript
-var ts = require("gulp-typescript");
-var tsProject = ts.createProject("tsconfig.json");
+// var ts = require("gulp-typescript");
+// var tsProject = ts.createProject("tsconfig.json");
 
-// Taks #0: default
-gulp.task("default", function () {
-  return tsProject.src()
-    .pipe(ts(tsProject))
-    .js.pipe(gulp.dest("dist"));
+// Browserify
+var browserify = require("browserify");
+var source = require("vinyl-source-stream");
+var tsify = require("tsify");
+var paths = { pages: ["src/*.html"] };
+
+// Task #copy-html
+gulp.task("copy-html", function() {
+  return gulp.src(paths.pages)
+    .pipe(gulp.dest("dist"));
+});
+
+// Task #default
+gulp.task("default", ["copy-html"], function () {
+  return browserify({
+    basedir: ".",
+    debug: false,
+    entries: ["src/main.ts"],
+    cache: {},
+    packageCache: {}
+  })
+    .plugin(tsify)
+    .bundle()
+    .pipe(source("bundle.js"))
+    .pipe(gulp.dest("dist"));
 });
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-function hello(compiler: string) {
-  console.log(`Hello from ${compiler}`);
+import {sayHello} from "./greet";
+
+function showHello(divName: string, name: string) {
+  const elt = document.getElementById(divName);
+  elt.innerText = sayHello(name);
 }
-hello("TypeScript");
+
+showHello("greeting", "TypeScript");
