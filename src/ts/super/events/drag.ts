98f1bd8ba886fca3f1435fadb4d521513a362dc7
Marco Arruda <marco.nc.arruda@gmail.com>
2016-07-26 14:50:32 -0300

    this is resizable!!!

--- src/ts/super/events/drag.ts
+++ src/ts/super/events/drag.ts
@@ -7,14 +7,16 @@ import {Geometry} from "../../types/Geometry.ts";
 import {EventsParent} from "./_parent.ts";
 
 export class DragEvents extends EventsParent {
-  
+
   constructor() {
     super();
-    this.DelegateEvent(".jsWidgetContainer[data-widget-movable='1']", "mousedown", this.MouseDown);
+    this.DelegateEvent(".jsWidgetContainer[data-widget-conf='1']", "mousedown", this.MouseDown);
     this.DelegateEvent(document, "mousemove", this.MouseMove);
-    this.DelegateEvent(".jsWidgetContainer[data-widget-movable='1']", "mouseup", this.MouseUp);
+    this.DelegateEvent(".jsWidgetContainer[data-widget-conf='1']", "mouseup", this.MouseUp);
   }
 
+  private toMove: Boolean;
+  private toResize: Boolean;
   private widgetInstanceId: number;
   private moveX: number;
   private moveY: number;
@@ -22,51 +24,82 @@ export class DragEvents extends EventsParent {
   private lastY: number;
 
   public MouseDown = (e?: MouseEvent) => {
+    if ($(e.toElement).hasClass("jsWidgetResize")) {
+      this.toMove = false;
+      this.toResize = true;
+    } else {
+      this.toMove = true;
+      this.toResize = false;
+    }
     this.widgetInstanceId = parseInt($(e.toElement).closest(".jsWidgetContainer").attr("data-widget-instance-id"));
     this.lastX = e.pageX;
     this.lastY = e.pageY;
   }
-
   public MouseMove = (e?: MouseEvent) => {
-    if(parseInt($(e.toElement).closest(".jsWidgetContainer").attr("data-widget-instance-id")) == this.widgetInstanceId) {
-      let dx: number = e.pageX - this.lastX;
-      let dy: number = e.pageY - this.lastY;
-
-      let left: number = parseInt($(".jsWidgetContainer[data-widget-instance-id='" + this.widgetInstanceId + "']").css("left"));
-      let top: number = parseInt($(".jsWidgetContainer[data-widget-instance-id='" + this.widgetInstanceId + "']").css("top"));
-
-      let pos : Geometry.Point2D = this._ApplyBoundaries({x: left+dx, y: top+dy});
-      $(".jsWidgetContainer[data-widget-instance-id='" + this.widgetInstanceId + "']").css("left", pos.x);
-      $(".jsWidgetContainer[data-widget-instance-id='" + this.widgetInstanceId + "']").css("top", pos.y);
-
-      this.lastX = e.pageX;
-      this.lastY = e.pageY;
+    if (parseInt($(e.toElement).closest(".jsWidgetContainer").attr("data-widget-instance-id")) == this.widgetInstanceId) {
+      if (this.toMove) {
+        this._WidgetMove(e);
+      }
+      if (this.toResize) {
+        this._WidgetResize(e);
+      }
     } else {
-      this.widgetInstanceId = 0;
+      if (this.toResize) {
+        this._WidgetResize(e);
+      } else {
+        this.widgetInstanceId = 0;
+      }
     }
-  }
 
+    this.lastX = e.pageX;
+    this.lastY = e.pageY;
+  }
   public MouseUp = (e?: MouseEvent) => {
     this.widgetInstanceId = 0;
+    this.toMove = this.toResize = false;
   }
 
-  private _ApplyBoundaries(pos : Geometry.Point2D) : Geometry.Point2D {
-    let offset : any = $(".jsTabContent.jsShow").offset();
+  private _WidgetResize(e: MouseEvent): void {
+    let d: Geometry.Point2D = this._GetMouseDistance(e);
+
+    let width: number = $(".jsWidgetContainer[data-widget-instance-id='" + this.widgetInstanceId + "']").width();
+    let height: number = $(".jsWidgetContainer[data-widget-instance-id='" + this.widgetInstanceId + "']").height();
+
+    let size: Geometry.Point2D = { x: width + d.x, y: height + d.y };
+    console.log(size);
+    $(".jsWidgetContainer[data-widget-instance-id='" + this.widgetInstanceId + "']").width(size.x);
+    $(".jsWidgetContainer[data-widget-instance-id='" + this.widgetInstanceId + "']").height(size.y);
+  }
+  private _WidgetMove(e: MouseEvent): void {
+    let d: Geometry.Point2D = this._GetMouseDistance(e);
+
+    let left: number = parseInt($(".jsWidgetContainer[data-widget-instance-id='" + this.widgetInstanceId + "']").css("left"));
+    let top: number = parseInt($(".jsWidgetContainer[data-widget-instance-id='" + this.widgetInstanceId + "']").css("top"));
+
+    let pos: Geometry.Point2D = this._ApplyBoundaries({ x: left + d.x, y: top + d.y });
+    $(".jsWidgetContainer[data-widget-instance-id='" + this.widgetInstanceId + "']").css("left", pos.x);
+    $(".jsWidgetContainer[data-widget-instance-id='" + this.widgetInstanceId + "']").css("top", pos.y);
+  }
+  private _GetMouseDistance(e: MouseEvent): Geometry.Point2D {
+    return { x: e.pageX - this.lastX, y: e.pageY - this.lastY };
+  }
+  private _ApplyBoundaries(pos: Geometry.Point2D): Geometry.Point2D {
+    let offset: any = $(".jsTabContent.jsShow").offset();
     let xMin: number = offset.left;
     let yMin: number = offset.top;
     let xMax: number = xMin + $(".jsTabContent.jsShow").width() - $(".jsWidgetContainer[data-widget-instance-id='" + this.widgetInstanceId + "']").width();
     let yMax: number = yMin + $(".jsTabContent.jsShow").height() - $(".jsWidgetContainer[data-widget-instance-id='" + this.widgetInstanceId + "']").height();
 
-    if(pos.x > xMax) {
+    if (pos.x > xMax) {
       pos.x = xMax;
     }
-    if(pos.x < xMin) {
+    if (pos.x < xMin) {
       pos.x = xMin;
     }
-    if(pos.y > yMax) {
+    if (pos.y > yMax) {
       pos.y = yMax;
     }
-    if(pos.y < yMin) {
+    if (pos.y < yMin) {
       pos.y = yMin;
     }
 
